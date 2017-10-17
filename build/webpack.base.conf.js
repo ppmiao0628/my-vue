106528edf90c6b9e8ea56146d852e114e0bc9836
mpp <ppmiao0628@outlook.com>
2017-10-17 20:25:50 +0800

    删除无用main.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,8 +10,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js',
-    main: './src/view/main/main.js'
+    app: './src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
