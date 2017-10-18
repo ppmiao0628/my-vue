a0f48ddc5af4e9bc3cc4584396ccd142f3616fe2
mpp <ppmiao0628@outlook.com>
2017-10-18 11:29:53 +0800

    checkbox and radio

--- src/view/test/App.vue
+++ src/view/test/App.vue
@@ -9,15 +9,39 @@
     <p>{{ total }}</p>
     <button-counter v-on:increment="incrementTotal"></button-counter>
     <button-counter v-on:increment="incrementTotal"></button-counter>
+    <br>
+    <input type="checkbox" id="1" value="jack" v-model="checkedMsg"/>
+    <label for="jack">jack</label>
+    <input type="checkbox" id="2" value="jone" v-model="checkedMsg"/>
+    <label for="jone">jone</label>
+    <input type="checkbox" id="3" value="jenny" v-model="checkedMsg"/>
+    <label for="jenny">jenny</label>
+    <br>
+    <span>Checked item : {{checkedMsg}} </span>
+    <br>
+    <input type="radio" id="4" value="jimmy" v-model="radioMsg"/>
+    <label for="jimmy">jimmy</label>
+    <input type="radio" id="5" value="jeck" v-model="radioMsg"/>
+    <label for="jeck">jeck</label>
+    <input type="radio" id="6" value="jine" v-model="radioMsg"/>
+    <label for="jine">jine</label>
+    <br>
+    <span>Checked item : {{radioMsg}} </span>
+    <hello-world></hello-world>
   </div>
 </template>
 
 <script>
+  import HelloWorld from '../../components/HelloWorld.vue'
+
   export default {
+    components: {HelloWorld},
     name: 'app',
     data () {
       return {
         parentMsg: '',
+        checkedMsg: [],
+        radioMsg: '',
         total: 0
       }
     },
